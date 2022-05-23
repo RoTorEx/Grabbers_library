af441da9249311750709a39b1dca0c51ff24bce9
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-05-23 14:14:26 +0300

    feat: add get_pages func
    Func get_pages collect count of pages on Lamoda site.

--- main.py
+++ main.py
@@ -4,12 +4,36 @@ import json
 import requests
 import time
 import datetime
+import pprint
 
 
 start_time = time.time()
 
 
-def get_data():
+def get_pages():
+    '''Method makes a request to the site to find the number of pages.'''
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Mobile Safari/537.36",
+        "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+    }
+
+    url = "https://www.lamoda.by/c/4152/default-men/?page=1&json=1"
+
+    response = requests.get(headers=headers, url=url)
+
+    jsoned_html = json.loads(response.content)
+
+    pages_count = jsoned_html['payload']['pagination']['pages']
+
+    # with open("json_page.json", "w") as file:
+    #     json.dump(jsoned_html, file, indent=4, ensure_ascii=False)
+
+    return pages_count
+
+
+def get_data(pages_count):
+    '''Method get gata from page'''
 
     cur_time = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
 
@@ -24,7 +48,7 @@ def get_data():
 
     print("Parsering in process...")
 
-    for page in range(1, 168):
+    for page in range(1, pages_count + 1):
         url += f"?page={page}"
 
         response = requests.get(headers=headers, url=url)
@@ -78,7 +102,7 @@ def get_data():
             )
 
         sys.stdout.write(u"\u001b[1000D")
-        sys.stdout.write(f"Pages parsered: ==< {page} >==")
+        sys.stdout.write(f"Pages parsered: ==< {page}/{pages_count} >==")
         sys.stdout.flush()
         time.sleep(.5)
 
@@ -87,9 +111,13 @@ def get_data():
 
 
 def main():
-    get_data()
+    pages_count = get_pages()
+    get_data(pages_count)
+
     finish_time = time.time() - start_time
-    print(f"\nElapsed time to script's work: {finish_time} seconds.")
+    spent_time = time.strftime("%M:%S (min:sec)", time.gmtime(finish_time))
+
+    print(f"\nElapsed time to script's work: {spent_time}.")
 
 
 if __name__ == "__main__":
