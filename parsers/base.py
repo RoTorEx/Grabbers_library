60b1bfbc3a3185a921acdb3a6bccebaecf35e8cc
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-05-30 15:19:05 +0300

    build: application extension
    Refactor structure using SOLID principles

--- parsers/base.py
+++ parsers/base.py
@@ -2,12 +2,22 @@ from typing import Optional, List
 import dataclasses
 import abc
 
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import sys
+import json
+import time
+import datetime
+import aiohttp
+import asyncio
+
 
 @dataclasses.dataclass
 class Entry:
-    price: int
     name: str
-    description: Optional[str]
+    price: str
+    link: Optional[str]
+    common: dict
 
 
 class BaseParser(abc.ABC):
@@ -17,6 +27,9 @@ class BaseParser(abc.ABC):
         "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
     }
     BASE_URL = NotImplemented
+    COLLECT_DATA = []
+    FINISH_PAGES = 0
+    TIME = {"start": 0, "end": 0}
 
     @abc.abstractmethod
     def parse(self, *args, **kwargs):
@@ -24,8 +37,35 @@ class BaseParser(abc.ABC):
 
     @abc.abstractmethod
     def clean(self, *args, **kwargs) -> List[Entry]:
-        pass
+
+        cur_time = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
+
+        with open(f"data/{cur_time}_items.json", "w") as file:
+            json.dump(self.COLLECT_DATA, file, indent=4, ensure_ascii=False)
+
+        self.TIME['end'] = time.time()
+        spent_time = (f"{self.TIME['end'] - self.TIME['start']:.4}")
+        print(f"\nElapsed time to script's work: {spent_time} seconds.")
 
     @abc.abstractmethod
-    def run(self, *args, **kwargs):
-        pass
+    def run(self, pages_count, *args, **kwargs):
+        self.TIME['start'] = time.time()
+
+        async def gather_data(self, url, headers, pages_count):
+            '''Asynchronous function which creates tasks-functions.'''
+
+            connector = aiohttp.TCPConnector(verify_ssl=False, limit=50)
+
+            async with aiohttp.ClientSession(connector=connector) as session:
+                tasks = []
+
+                for page in range(1, pages_count + 1):
+                    task = asyncio.create_task(self.parse(session, page, pages_count, headers, url))
+                    tasks.append(task)
+
+                await asyncio.gather(*tasks)
+
+                self.clean()
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(gather_data(self, self.BASE_URL, self.BASE_HEADERS, pages_count))
--- runner.py
+++ runner.py
@@ -1,7 +1,41 @@
-from parsers.base import BaseParser
+from pathlib import Path
+import os
+
+from parsers.lamoda import LamodaParser
+
+
+def main():
+    site = ParserRunner()
+    site.run(site.site_parser)
 
 
 class ParserRunner:
 
-    def run(parser: BaseParser):
-        result = parser.run()
+    __slots__ = ("site_name", "site_parser", "pages")
+
+    PARSERS = {
+        "lamoda": LamodaParser()
+    }
+
+    def __init__(self):
+        parsers_list = (map(lambda file: file.strip('.py'),
+                            filter(lambda file: file not in ['__init__.py', '__pycache__', 'base.py'],
+                                   os.listdir(f"{Path.cwd()}/parsers"))))
+
+        self.site_name = input("Enter site name, please: ").strip().split('.')[0]
+        self.pages = int(input("Enter count of pages which you like to parse: "))
+
+        if self.site_name in parsers_list:
+            self.site_parser = self.PARSERS[self.site_name]
+
+            # self.run(self.site_parser)  # Creating an instance of a class
+
+        else:
+            raise AttributeError("There is no ready parser for this site yet. Or check the site name and try again.")
+
+    def run(self, parser):
+        parser.run(self, self.pages)  # Run method of parser-class instance
+
+
+if __name__ == "__main__":
+    main()
